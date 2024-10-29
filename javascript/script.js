26e2042c3cbcf52bd235cb23fd2f7bf141ae49a1
ScopeTech16 <sodiqomisanya313@gmail.com>
2024-10-29 12:49:46 +0000

    alert message updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,14 @@
 const texts = ["talent showcase opportunities", "centralized communication", "networking opportunities"];
 let index = 0;
 let charIndex = 0;
-const typingSpeed = 15;  // Speed of typing and erasing (faster typing)
-const delayBetweenTypingAndErasing = 1000; // Pause before erasing starts
-const clearTypingDelay = 40; // Delay after clearing before typing starts
+const typingSpeed = 15; 
+const delayBetweenTypingAndErasing = 1000;
+const clearTypingDelay = 40; 
 
 const typewriterElement = document.getElementById("typewriter");
 
 function typeText() {
   if (charIndex < texts[index].length) {
-    // Type letters from the center outward
     typewriterElement.textContent = texts[index].substring(0, charIndex + 1);
     charIndex++;
     setTimeout(typeText, typingSpeed);
@@ -29,36 +28,36 @@ function eraseText() {
     charIndex--;
     setTimeout(eraseText, typingSpeed);
   } else {
-    // Add a 15 ms delay before starting to type the next word
+    
     setTimeout(() => {
-      index = (index + 1) % texts.length;  // Move to the next word
-      typeText();  // Start typing the next word
+      index = (index + 1) % texts.length;  
+      typeText();  
     }, clearTypingDelay);
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(typeText, 1000);  // Start typing after a brief delay
+  setTimeout(typeText, 1000);  
 });
 
 
 
 // Waitlist form
 document.getElementById("waitlistForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+    event.preventDefault(); 
 
     const submitButton = document.getElementById("submitButton");
     const formData = new FormData(this);
-    const scriptURL = "https://script.google.com/macros/s/AKfycbxFJo6iuHM3YRPeVmVQjBHj6hI9QmVv9J5yj8rqMYcGPehNU3WlaxSxjNnVkJay2SNdqg/exec"; // Replace with your Apps Script Web App URL
+    const scriptURL = "https://script.google.com/macros/s/AKfycbyt5pWT8agKiN7Xm82fFPDTfeIK9sEliTFV0ZyN8nYDrPq7Xk7C5AVO2unHPRY0L4zn/exec"; 
 
-    // Change button text to "Loading..."
+
     submitButton.disabled = true;
     submitButton.textContent = "Loading...";
 
     fetch(scriptURL, { method: "POST", body: formData })
         .then(response => {
             if (response.ok) {
-                showSuccessMessage(); // Show success message
+                showSuccessMessage(); 
             } else {
                 alert("There was a problem submitting the form. Please try again.");
             }
